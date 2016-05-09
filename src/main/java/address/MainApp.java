6f23d9305a3d1789d98fa216884c5b5445283a22
Damith C. Rajapakse <damith@gmail.com>
2016-05-09 12:22:09 +0800

    Shutdown everything if closed using x button If not, the thread keeps going.

--- src/main/java/address/MainApp.java
+++ src/main/java/address/MainApp.java
@@ -46,6 +46,11 @@ public class MainApp extends Application {
         mainController.start(primaryStage);
     }
 
+    @Override
+    public void stop(){
+        System.exit(0);
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
