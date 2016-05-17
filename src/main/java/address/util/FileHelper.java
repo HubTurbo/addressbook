14e09c074aee1ec89ea0d72565e355a84e949500
Hans Adrian <nick.hansadrian@gmail.com>
2016-05-17 16:25:23 +0800

    Added a file helper

---
+++ src/main/java/address/util/FileHelper.java
@@ -0,0 +1,92 @@
+package address.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Writes and reads file
+ */
+public class FileHelper {
+    private static final String CHARSET = "UTF-8";
+
+    /**
+     * Creates a file and its parent directories if it does not exists
+     *
+     * @return true if file is created, false if file already exists
+     */
+    public static boolean createFile(File file) throws IOException {
+        if (file.exists()) {
+            return false;
+        }
+
+        File parentDir = file.getParentFile();
+
+        if (parentDir != null && !parentDir.exists() && parentDir.mkdirs()) {
+            throw new IOException("Failed to make parent directories of file");
+        }
+
+        return file.createNewFile();
+    }
+
+    /**
+     * Lists all files in directory and its subdirectories
+     */
+    public static List<File> listFilesInDir(Path directory) {
+        List<File> filepaths = new ArrayList<>();
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
+            for (Path path : stream) {
+                if (path.toFile().isDirectory()) {
+                    filepaths.addAll(listFilesInDir(path));
+                } else {
+                    filepaths.add(path.toFile());
+                }
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return filepaths;
+    }
+
+    public static void moveFile(Path source, Path dest, boolean isOverwrite) throws IOException {
+        if (isOverwrite) {
+            Files.move(source, dest, StandardCopyOption.REPLACE_EXISTING);
+        } else {
+            Files.move(source, dest);
+        }
+    }
+
+    /**
+     * @return List of filenames failed to be moved
+     */
+    public static List<String> moveContentOfADirectoryToAnother(String sourceDir, String targetDir) {
+        List<File> sourceFiles = FileHelper.listFilesInDir(Paths.get(sourceDir));
+        List<String> failedToMoveFiles = new ArrayList<>();
+
+        for (File sourceFile : sourceFiles) {
+            Path sourceFilePath = sourceFile.toPath();
+            Path targetFilePath = Paths.get(targetDir + File.separator + sourceFile.getName());
+
+            try {
+                FileHelper.moveFile(sourceFilePath, targetFilePath, true);
+            } catch (IOException e) {
+                e.printStackTrace();
+                failedToMoveFiles.add(sourceFile.getPath());
+            }
+        }
+
+        return failedToMoveFiles;
+    }
+
+    public static String readFromFile(File file) throws IOException {
+        String contents = new String(Files.readAllBytes(file.toPath()), CHARSET);
+        return contents;
+    }
+
+    public static void writeToFile(File file, String content) throws IOException {
+        Files.write(file.toPath(), content.getBytes(CHARSET));
+    }
+}
