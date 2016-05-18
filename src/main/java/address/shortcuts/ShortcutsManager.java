2d06a46c70869fac2af4f969819e686251630953
Damith C. Rajapakse <damith@gmail.com>
2016-05-18 18:46:17 +0800

    Added Shortcut class

--- src/main/java/address/shortcuts/ShortcutsManager.java
+++ src/main/java/address/shortcuts/ShortcutsManager.java
@@ -5,6 +5,9 @@ import com.google.common.eventbus.Subscribe;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCodeCombination;
 import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+
+import java.util.*;
 
 /**
  * Manages keyboard shortcuts
@@ -20,21 +23,58 @@ public class ShortcutsManager {
     public static final KeyCombination SHORTCUT_PERSON_EDIT = new KeyCodeCombination(KeyCode.T);
     public static final KeyCombination SHORTCUT_JUMP_TO_LIST = new KeyCodeCombination(KeyCode.DOWN, KeyCombination.CONTROL_DOWN);
 
+    private List<Shortcut> shortcuts = new ArrayList<>();
+
+    public ShortcutsManager(){
+        initShortcuts();
+    }
+
+    private void initShortcuts(){
+        addShortcut(KeyCode.D,
+                ()-> EventManager.getInstance().post(new DeleteRequestEvent()));
+        addShortcut(KeyCode.E,
+                ()-> EventManager.getInstance().post(new EditRequestEvent()));
+        addShortcut(KeyCode.DOWN, KeyCombination.CONTROL_DOWN,
+                ()-> EventManager.getInstance().post(new JumpToListRequestEvent()));
+
+    }
+
+    private void addShortcut(KeyCode mainKey, KeyCombination.Modifier modifierKey, Runnable action) {
+        shortcuts.add(new Shortcut(new KeyCodeCombination(mainKey, modifierKey),action));
+    }
+
+    private void addShortcut(KeyCode mainKey, Runnable action) {
+        shortcuts.add(new Shortcut(new KeyCodeCombination(mainKey),action));
+    }
+
+    private Optional<Runnable> getAction(KeyEvent keyEvent){
+       Optional<Shortcut> matchingShortcut =
+               shortcuts
+                .stream()
+                .filter(shortcut -> shortcut.keyCombination.match(keyEvent))
+                .findFirst();
+        return Optional.ofNullable(matchingShortcut.isPresent()? matchingShortcut.get().action : null);
+    }
+
     @Subscribe
     public void handlePotentialKeyboardShortcutEvent(PotentialKeyboardShortcutEvent potentialKeyboardShortcutEvent) {
 
-        if(SHORTCUT_JUMP_TO_LIST.match(potentialKeyboardShortcutEvent.keyEvent)){
-            EventManager.getInstance().post(new JumpToListRequestEvent());
-            return;
-        }
-
-        switch (potentialKeyboardShortcutEvent.keyEvent.getCode()) {
-            case D:
-                EventManager.getInstance().post(new DeleteRequestEvent()); break;
-            case E:
-                EventManager.getInstance().post(new EditRequestEvent()); break;
-            default:
-                System.out.println("Unknown shortcut");
+        Optional<Runnable> action = getAction(potentialKeyboardShortcutEvent.keyEvent);
+        if (action.isPresent()) {
+            action.get().run();
+        } else {
+            System.out.println("No action for shortcut " + potentialKeyboardShortcutEvent.keyEvent);
         }
     }
+
+    private class Shortcut{
+        KeyCombination keyCombination;
+        Runnable action;
+
+        Shortcut(KeyCombination keyCombination, Runnable action){
+            this.keyCombination = keyCombination;
+            this.action = action;
+        }
+    }
+
 }
