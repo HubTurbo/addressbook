3b2bab36f6ba1803a73d000edda6c9d0c1bd7c91
m133225 <m133225@hotmail.com>
2016-05-20 18:18:20 +0800

    Temporarily disabled dialog popup. Fixed tests but they still require a default xml file to run. Added the default data file path back to git.

--- src/test/java/address/guitests/GroupEditDialogTest.java
+++ src/test/java/address/guitests/GroupEditDialogTest.java
@@ -8,17 +8,16 @@ import static org.testfx.api.FxAssert.verifyThat;
 public class GroupEditDialogTest extends GuiTestBase {
     @Test
     public void groupEditDialogLoadTest() {
-        clickOn("Groups").clickOn("New Group");
+        // this test currently assumes that the default data file exists
+        clickOn("File").clickOn("Append Sample Data")
+                .clickOn("Groups").clickOn("New Group");
 
-        //findStageByTitle("Edit Group");
         verifyThat("#groupNameField", (TextField t) -> t.getText().equals(""));
 
         clickOn("Cancel").clickOn("Groups").clickOn("Manage Groups")
+                .doubleClickOn(targetWindow("List of Contact Groups").lookup("friends").tryQuery().get());
 
-        // to be used after we can load from a custom data file from config
-        .doubleClickOn(getWindowNode("List of Contact Groups").lookup("friends").tryQuery().get());
-
-        findStageByTitle("Edit Group");
+        targetWindow("Edit Group");
         verifyThat("#groupNameField", (TextField t) -> t.getText().equals("friends"));
     }
 }
