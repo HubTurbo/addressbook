38ac2df75934e4ba9edb780a10916bd1e91fbaf0
Leow Yijin <leowyijin@gmail.com>
2016-06-06 17:03:44 +0800

    added base Viewable class to serve as common ancestor to all Viewable*

---
+++ src/main/java/address/model/datatypes/Viewable.java
@@ -0,0 +1,90 @@
+package address.model.datatypes;
+
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.WritableValue;
+
+import java.util.function.Function;
+
+/**
+ * Common functionality among all ViewableX classes:
+ * eg.  - the ability to deactivate and reactivate syncing visible data with backing data (for optimistic ui updating)
+ *      - common fields/references and constructor logic
+ *      - maintains visible state separately from canonical state (see {@link #visible}, {@link #backing})
+ */
+public abstract class Viewable<D extends BaseDataType> {
+
+    protected final D visible;
+    protected D backing;
+    protected boolean isSyncingWithBackingObject;
+
+    /**
+     * Create a new Viewable based on a backing object.
+     * @param backingObject
+     */
+    protected Viewable(D backingObject, Function<D, D> visibleObjectGenerator) {
+        backing = backingObject;
+        visible = visibleObjectGenerator.apply(backingObject);
+        conditionallyBindVisibleToBacking();
+        isSyncingWithBackingObject = true;
+    }
+
+    /**
+     * Make every relevant data field inside the {@link #visible} object track and mirror the {@link #backing} object,
+     * as long as {@link #isSyncingWithBackingObject} is true.
+     *
+     * Consider using the {@link #conditionallyBindValue(ObservableValue, WritableValue)} helper method to make
+     * changes to {@code ObservableValue} fields in {@link #backing} propagate to {@code WritableValue} fields in
+     * {@link #visible} only when {@link #isSyncingWithBackingObject} is true.
+     *
+     * @see #conditionallyBindValue(ObservableValue, WritableValue)
+     * @see #visible
+     * @see #backing
+     */
+    protected abstract void conditionallyBindVisibleToBacking();
+
+    /**
+     * Binds {@code listener} to {@code dependency} such that ONLY whenever {@code dependency} changes AND
+     * {@link #isSyncingWithBackingObject} is true, then {@code listener} gets updated.
+     *
+     * @param dependency notifies {@code listener} of any changes.
+     * @param listener will be updated whenever {@code dependency} changes AND {@link #isSyncingWithBackingObject} is true.
+     * @param <T> ensures both arguments have same type parameter.
+     */
+    protected <T> void conditionallyBindValue(ObservableValue<T> dependency, WritableValue<T> listener) {
+        dependency.addListener((dep, oldValue, newValue) -> {
+            if (this.isSyncingWithBackingObject) {
+                listener.setValue(newValue);
+            }
+        });
+    }
+
+
+    /**
+     * @return true if changes to the backing object AFTER this method call will propagate to the visible object.
+     */
+    public boolean isSyncingWithBackingObject() {
+        return isSyncingWithBackingObject;
+    }
+
+    /**
+     * Changes in the backing object will be stored but not visible.
+     */
+    public void stopSyncingWithBackingObject() {
+        isSyncingWithBackingObject = false;
+    }
+
+    /**
+     * Future changes to the backing object will propagate to the visible object.
+     * NOTE: If backing and visible states have diverged, will NOT force a convergence.
+     *       Each visible object's field will only converge when the corresponding backing object field is changed.
+     */
+    public void continueSyncingWithBackingObject() {
+        isSyncingWithBackingObject = true;
+    }
+
+    /**
+     * Forcibly updates visible state to match current backing state
+     */
+    public abstract void forceSyncFromBacking();
+
+}
