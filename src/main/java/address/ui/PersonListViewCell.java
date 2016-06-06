cdd92013443e777be4f9a449216db387e3fb0645
You Liang <a0125975@u.nus.edu>
2016-06-06 17:54:03 +0800

    Remove animation pack

--- src/main/java/address/ui/PersonListViewCell.java
+++ src/main/java/address/ui/PersonListViewCell.java
@@ -1,27 +1,12 @@
 package address.ui;
 
-import address.animation.AnimationPack;
 import address.controller.PersonCardController;
-
 import address.model.datatypes.Person;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Platform;
-
 import javafx.scene.control.ListCell;
-import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
-import javafx.util.Duration;
-
-import java.util.concurrent.TimeUnit;
 
 public class PersonListViewCell extends ListCell<Person> {
-
-
-    protected AnimationPack anim;
 
     @Override
     public void updateItem(Person person, boolean empty) {
@@ -40,19 +25,5 @@ public class PersonListViewCell extends ListCell<Person> {
             pane.prefWidthProperty().bind(this.widthProperty());
         }
     }
-
-
-
-    private void animate() {
-        if (anim != null && anim.getKeyFrames().size() >= 0
-                && (anim.getTimeline().getStatus() == Timeline.Status.STOPPED
-                || anim.getTimeline().getStatus() == Timeline.Status.PAUSED)) {
-            anim.getTimeline().playFromStart();
-//            if (oldIndex < getIndex() && Math.abs(oldIndex - getIndex()) == 1) {
-//                anim.getTimeline().playFromStart();
-//            }
-        }
-    }
-
 }
 
