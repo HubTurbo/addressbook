HubTurbo/addressbook

commit 0685541fb2152d4bba10fe3cd94a7a2067814764

[#170] Implement ViewableModel -- Model dual layer (PR#121)
* added field iterating methods to ObservablePerson interface

* removed forEachObservableListFieldWith method since it's too difficult to handle each possible obslist type parameter

* added base Viewable class to serve as common ancestor to all Viewable*

* added observable extractor to Person's tag list so ListChangeListeners can raise update change events

* added observable field extracting capablities to domain model datatypes

* improved comments on Viewable

* Added ViewablePerson implementation and pending-countdown property

* simple viewableaddressbook implementation

* separated publicly visible addressbook from the hidden visible/backing model in the Model component

* Data model based interfaces for ModelManager

* added model exposing interfaces to modelmanager

* added utility methods to upcast collections of X to collections of ?superX

* fixed and linked up browser component to new viewablePerson system

* fix breaks, made UI and browser use viewablePerson's minimal required interfaces

* fixed broken tests

* fixing delete bug at listview

* removed unecessary optional

* incorrect use of weak listener bugfixed

* changed modelmanager to return unmodifiable views to external components

* removed WritablePerson interface since only Model component will use it

* git merge backingaddressbook, and restructure packages

* merge readableX and observableX into readonlyX

* added ReadOnly interfaces to ViewableAB and AB and ModelManager

* fix style issues

* fixed failing serialisation test

* finalised methods in unmodifiableObsList

* shifted base viewable class out to common datatype package

* made properties from readonlyX unmodifiable

src/main/java/address/browser/BrowserManager.java
@@ -2,7 +2,7 @@ package address.browser;
 
 import address.events.EventManager;
 import address.events.LocalModelChangedEvent;
-import address.model.datatypes.Person;
+import address.model.datatypes.person.ReadOnlyViewablePerson;
 
 import com.google.common.eventbus.Subscribe;
 
@@ -22,22 +22,21 @@ import java.util.logging.Level;
  */
 public class BrowserManager {
 
-
-    public static final int NUMBER_OF_PRELOADED_PAGE = 3;
+    public static final int NUMBER_OF_PRELOADED_PAGES = 3;
     public static final int PERSON_NOT_FOUND = -1;
 
     private Optional<AddressBookBrowser> browser;
 
-    private ObservableList<Person> filteredPersons;
+    private ObservableList<ReadOnlyViewablePerson> persons;
 
-    public BrowserManager(ObservableList<Person> filteredPersons) {
-        this.filteredPersons = filteredPersons;
+    public BrowserManager(ObservableList<ReadOnlyViewablePerson> persons) {
+        this.persons = persons;
         String headlessProperty = System.getProperty("testfx.headless");
         if (headlessProperty != null && headlessProperty.equals("true")) {
             browser = Optional.empty();
             return;
         }
-        browser = Optional.of(new AddressBookBrowser(NUMBER_OF_PRELOADED_PAGE, this.filteredPersons));
+        browser = Optional.of(new AddressBookBrowser(NUMBER_OF_PRELOADED_PAGES, this.persons));
         browser.get().registerListeners();
         EventManager.getInstance().registerHandler(this);
     }
@@ -56,13 +55,13 @@ public class BrowserManager {
      * Updates the browser contents.
      */
     private void updateBrowserContent() {
-        List<Person> personsInBrowserCache = browser.get().getPersonsLoadedInCache();
+        List<ReadOnlyViewablePerson> personsInBrowserCache = browser.get().getPersonsLoadedInCache();
         personsInBrowserCache.stream().forEach(person -> {
-                if (filteredPersons.indexOf(person) == PERSON_NOT_FOUND){
+                if (persons.indexOf(person) == PERSON_NOT_FOUND){
                     browser.get().unloadProfilePage(person);
                 } else {
-                    int indexOfContact = filteredPersons.indexOf(person);
-                    Person updatedPerson = filteredPersons.get(indexOfContact);
+                    int indexOfContact = persons.indexOf(person);
+                    ReadOnlyViewablePerson updatedPerson = persons.get(indexOfContact);
 
                     if (!updatedPerson.getGithubUserName().equals(person.getGithubUserName())){
                         browser.get().unloadProfilePage(person);
@@ -83,7 +82,7 @@ public class BrowserManager {
      * Loads the person's profile page to the browser.
      * PreCondition: filteredModelPersons.size() >= 1
      */
-    public void loadProfilePage(Person person){
+    public void loadProfilePage(ReadOnlyViewablePerson person){
         if (!browser.isPresent()) return;
         browser.get().loadProfilePage(person);
     }

src/main/java/address/controller/MainController.java
@@ -2,9 +2,10 @@ package address.controller;
 
 import address.MainApp;
 import address.events.*;
-import address.model.datatypes.Tag;
+import address.model.datatypes.person.ReadOnlyViewablePerson;
+import address.model.datatypes.person.ReadOnlyPerson;
+import address.model.datatypes.tag.Tag;
 import address.model.ModelManager;
-import address.model.datatypes.Person;
 import address.util.Config;
 import address.browser.BrowserManager;
 
@@ -58,7 +59,7 @@ public class MainController {
         this.modelManager = modelManager;
         this.config = config;
         this.mainApp = mainApp;
-        this.browserManager = new BrowserManager(modelManager.getFilteredPersons());
+        this.browserManager = new BrowserManager(modelManager.getAllViewablePersonsReadOnly());
     }
 
     public void start(Stage primaryStage) {
@@ -173,7 +174,7 @@ public class MainController {
      * @return a defensively copied optional containing the input data from user, or an empty optional if the
      *          operation is to be cancelled.
      */
-    public Optional<Person> getPersonDataInput(Person defaultData) {
+    public Optional<ReadOnlyPerson> getPersonDataInput(ReadOnlyPerson defaultData) {
         return showPersonEditDialog(defaultData);
     }
 
@@ -185,7 +186,7 @@ public class MainController {
      * @return an optional containing the new data, or an empty optional if there was an error
      *         creating the dialog or the user clicked cancel
      */
-    private Optional<Person> showPersonEditDialog(Person initialData) {
+    private Optional<ReadOnlyPerson> showPersonEditDialog(ReadOnlyPerson initialData) {
         final String fxmlResourcePath = FXML_PERSON_EDIT_DIALOG;
         try {
             // Load the fxml file and create a new stage for the popup dialog.
@@ -206,7 +207,7 @@ public class MainController {
             PersonEditDialogController controller = loader.getController();
             controller.setDialogStage(dialogStage);
             controller.setInitialPersonData(initialData);
-            controller.setTagsModel(modelManager.getTags(), new ArrayList<>(initialData.getTags()));
+            controller.setTagsModel(modelManager.getAllTags(), new ArrayList<>(initialData.getTags()));
 
             dialogStage.showAndWait();
             if (controller.isOkClicked()) {
@@ -315,7 +316,7 @@ public class MainController {
 
             // Set the persons into the controller.
             BirthdayStatisticsController controller = loader.getController();
-            controller.setPersonData(modelManager.getFilteredPersons());
+            controller.setPersonData(modelManager.getAllViewablePersonsReadOnly());
 
             dialogStage.show();
         } catch (IOException e) {
@@ -382,7 +383,7 @@ public class MainController {
         browserManager.freeBrowserResources();
     }
 
-    public void loadGithubProfilePage(Person person){
+    public void loadGithubProfilePage(ReadOnlyViewablePerson person){
         browserManager.loadProfilePage(person);
     }
 

src/main/java/address/model/datatypes/person/Person.java
@@ -1,8 +1,11 @@
-package address.model.datatypes;
+package address.model.datatypes.person;
 
+import address.model.datatypes.BaseDataType;
+import address.model.datatypes.tag.Tag;
 import address.util.DateTimeUtil;
 import address.util.LocalDateAdapter;
 
+import address.util.collections.UnmodifiableObservableList;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
@@ -17,6 +20,7 @@ import javafx.collections.ObservableList;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.function.BiConsumer;
 
 /**
  * Data-model implementation class representing the "Person" domain object.
@@ -25,9 +29,9 @@ import java.util.Optional;
  * Instead, use and declare the minimum required superclass/interface.
  *
  * Eg. A GUI element controller that only needs access to the Person's properties should declare the received Person
- * as an ObservablePerson -- since it does not need the functionality in the other superclasses/interfaces.
+ * as an ReadOnlyPerson -- since it does not need the functionality in the other superclasses/interfaces.
  */
-public class Person extends BaseDataType implements ReadablePerson, WritablePerson, ObservablePerson {
+public class Person extends BaseDataType implements ReadOnlyPerson {
 
     @JsonIgnore private final SimpleStringProperty firstName;
     @JsonIgnore private final SimpleStringProperty lastName;
@@ -40,8 +44,6 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
     @JsonIgnore private final SimpleObjectProperty<LocalDate> birthday;
     @JsonIgnore private final ObservableList<Tag> tags;
 
-    @JsonIgnore private final BooleanProperty isDeleted;
-    @JsonIgnore private final BooleanProperty isEdited;
     // defaults
     {
         firstName = new SimpleStringProperty("");
@@ -55,9 +57,6 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
         birthday = new SimpleObjectProperty<>();
 
         tags = FXCollections.observableArrayList();
-
-        isDeleted = new SimpleBooleanProperty(false);
-        isEdited = new SimpleBooleanProperty(false);
     }    
     
     /**
@@ -83,20 +82,18 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
     /**
      * Deep copy constructor.
      *
-     * @see Person#update(ReadablePerson)
+     * @see Person#update(ReadOnlyPerson)
      */
-    public Person(ReadablePerson toBeCopied) {
+    public Person(ReadOnlyPerson toBeCopied) {
         update(toBeCopied);
     }
 
     /**
      * {@inheritDoc}
      *
-     * @see WritablePerson#update(ReadablePerson)
      * @return self (calling this from a Person returns a Person instead of just a WritablePerson)
      */
-    @Override
-    public Person update(ReadablePerson newDataSource) {
+    public Person update(ReadOnlyPerson newDataSource) {
         setFirstName(newDataSource.getFirstName());
         setLastName(newDataSource.getLastName());
 
@@ -110,6 +107,27 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
         return this;
     }
 
+    // TODO: consider using reflection to access all isassignablefrom(Property) returning methods for maintainability
+    /**
+     * Passes matching property field pairs (paired between self and another ReadOnlyPerson) as arguments to a callback.
+     * The callback is called once for each property field in the ObservabePerson class.
+     *
+     * @param other the ReadOnlyPerson whose property fields make up the second parts of the property pairs
+     * @param action called for every property field: action(self:property, other:same_property)
+     *               first argument is from self, second is from the "other" parameter
+     */
+    public void forEachPropertyFieldPairWith(Person other, BiConsumer<? super Property, ? super Property> action) {
+        action.accept(firstName, other.firstName);
+        action.accept(lastName, other.lastName);
+        action.accept(githubUserName, other.githubUserName);
+
+        action.accept(street, other.street);
+        action.accept(postalCode, other.postalCode);
+        action.accept(city, other.city);
+
+        action.accept(birthday, other.birthday);
+    }
+
 //// NAME
 
     @JsonProperty("firstName")
@@ -118,13 +136,12 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
         return firstName.get();
     }
 
-    @Override
     public void setFirstName(String firstName) {
         this.firstName.set(firstName);
     }
 
     @Override
-    public StringProperty firstNameProperty() {
+    public ReadOnlyStringProperty firstNameProperty() {
         return firstName;
     }
 
@@ -134,13 +151,12 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
         return lastName.get();
     }
 
-    @Override
     public void setLastName(String lastName) {
         this.lastName.set(lastName);
     }
 
     @Override
-    public StringProperty lastNameProperty() {
+    public ReadOnlyStringProperty lastNameProperty() {
         return lastName;
     }
 
@@ -157,24 +173,24 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
         return githubUserName.get();
     }
 
-    @Override
     public void setGithubUserName(String githubUserName) {
         this.githubUserName.set(githubUserName);
     }
 
     @Override
-    public StringProperty githubUserNameProperty() {
+    public ReadOnlyStringProperty githubUserNameProperty() {
         return githubUserName;
     }
 
     @Override
-    public String profilePageUrl(){
+    public String githubProfilePageUrl(){
         return "https://www.github.com/" + githubUserName.get();
     }
 
-    public Optional<String> getGithubProfilePicUrl() {
+    @Override
+    public Optional<String> githubProfilePicUrl() {
         if (getGithubUserName().length() > 0) {
-            String profilePicUrl = profilePageUrl() + ".png";
+            String profilePicUrl = githubProfilePageUrl() + ".png";
             if (URLUtil.isURIFormat(profilePicUrl)){
                 return Optional.of(profilePicUrl);
             }
@@ -189,13 +205,12 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
         return street.get();
     }
 
-    @Override
     public void setStreet(String street) {
         this.street.set(street);
     }
 
     @Override
-    public StringProperty streetProperty() {
+    public ReadOnlyStringProperty streetProperty() {
         return street;
     }
 
@@ -207,13 +222,12 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
         return postalCode.get();
     }
 
-    @Override
     public void setPostalCode(String postalCode) {
         this.postalCode.set(postalCode);
     }
 
     @Override
-    public StringProperty postalCodeProperty() {
+    public ReadOnlyStringProperty postalCodeProperty() {
         return postalCode;
     }
 
@@ -225,13 +239,12 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
         return city.get();
     }
 
-    @Override
     public void setCity(String city) {
         this.city.set(city);
     }
 
     @Override
-    public StringProperty cityProperty() {
+    public ReadOnlyStringProperty cityProperty() {
         return city;
     }
 
@@ -245,13 +258,12 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
     }
 
     @JsonSetter("birthday")
-    @Override
     public void setBirthday(LocalDate birthday) {
         this.birthday.set(birthday);
     }
 
     @Override
-    public ObjectProperty<LocalDate> birthdayProperty() {
+    public ReadOnlyObjectProperty<LocalDate> birthdayProperty() {
         return birthday;
     }
 
@@ -266,10 +278,9 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
     @JsonProperty("tags")
     @Override
     public ObservableList<Tag> getTags() {
-        return tags;
+        return new UnmodifiableObservableList<>(tags);
     }
 
-    @Override
     public void setTags(Collection<Tag> tags) {
         this.tags.clear();
         this.tags.addAll(tags);
@@ -288,38 +299,20 @@ public class Person extends BaseDataType implements ReadablePerson, WritablePers
     }
 
 //// OTHER LOGIC
-    public boolean getIsDeleted() {
-        return isDeleted.get();
-    }
-
-    public BooleanProperty isDeletedProperty() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(boolean isDeleted) {
-        this.isDeleted.set(isDeleted);
-    }
-
-    public boolean getIsEdited() {
-        return isEdited.get();
-    }
-
-    public BooleanProperty isEditedProperty() {
-        return isEdited;
-    }
-
-    public void setIsEdited(boolean isEdited) {
-        this.isEdited.set(isEdited);
-    }
 
     @Override
-    public boolean equals(Object otherPerson){
-        if (otherPerson == this) return true;
-        if (otherPerson == null) return false;
-        if (!Person.class.isAssignableFrom(otherPerson.getClass())) return false;
-
-        final Person other = (Person) otherPerson;
-        return this.getFirstName().equals(other.getFirstName()) && this.getLastName().equals(other.getLastName());
+    public boolean equals(Object other) {
+        if (other == this) return true;
+        if (other == null) return false;
+        if (Person.class.isAssignableFrom(other.getClass())) {
+            final ReadOnlyPerson otherPerson = (ReadOnlyPerson) other;
+            return this.getFirstName().equals(otherPerson.getFirstName())
+                    && this.getLastName().equals(otherPerson.getLastName());
+        }
+        if (ViewablePerson.class.isAssignableFrom(other.getClass())) {
+            return other.equals(this);
+        }
+        return false;
     }
 
     @Override

src/main/java/address/model/datatypes/person/ReadOnlyPerson.java
@@ -1,0 +1,80 @@
+package address.model.datatypes.person;
+
+import address.model.datatypes.ExtractableObservables;
+import address.model.datatypes.tag.Tag;
+import javafx.beans.Observable;
+import javafx.beans.property.Property;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.collections.ObservableList;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
+/**
+ * Allows access to the Person domain object's data as javafx properties and collections for easy binding and listening.
+ * Also includes useful methods for working with fields from two ObservabblePersons together.
+ */
+public interface ReadOnlyPerson extends ExtractableObservables {
+
+    String getFirstName();
+    String getLastName();
+    /**
+     * @return first-last format full name
+     */
+    String fullName();
+
+    String getGithubUserName();
+    /**
+     * @return github profile url
+     */
+    String githubProfilePageUrl();
+    Optional<String> githubProfilePicUrl();
+
+    String getStreet();
+    String getPostalCode();
+    String getCity();
+
+    LocalDate getBirthday();
+    /**
+     * @return birthday date-formatted as string
+     */
+    String birthdayString();
+
+    /**
+     * @return string representation of this Person's tags
+     */
+    String tagsString();
+
+    ReadOnlyStringProperty firstNameProperty();
+    ReadOnlyStringProperty lastNameProperty();
+    ReadOnlyStringProperty githubUserNameProperty();
+
+    ReadOnlyStringProperty streetProperty();
+    ReadOnlyStringProperty postalCodeProperty();
+    ReadOnlyStringProperty cityProperty();
+
+    ReadOnlyObjectProperty<LocalDate> birthdayProperty();
+
+    /**
+     * @return ObservableList unmodifiable view of this Person's tags
+     */
+    ObservableList<Tag> getTags();
+
+    @Override
+    default Observable[] extractObservables() {
+        return new Observable[] {
+                firstNameProperty(),
+                lastNameProperty(),
+                githubUserNameProperty(),
+
+                streetProperty(),
+                postalCodeProperty(),
+                cityProperty(),
+
+                birthdayProperty(),
+                getTags()
+        };
+    }
+}

commit 3e4689613cc4d92b3579f1a13eb7ca52b188412c

Fix merge conflicts caused by PR #170

src/main/java/address/browser/BrowserManager.java
@@ -5,8 +5,8 @@ import address.browser.page.GithubProfilePage;
 import address.events.EventManager;
 import address.events.LocalModelChangedEvent;
 import address.exceptions.IllegalArgumentSizeException;
-import address.model.datatypes.Person;
 
+import address.model.datatypes.person.ReadOnlyViewablePerson;
 import address.util.UrlUtil;
 import com.google.common.eventbus.Subscribe;
 
@@ -32,11 +32,11 @@ public class BrowserManager {
 
     private static final String FXML_BROWSER_PLACE_HOLDER_SCREEN = "/view/DefaultBrowserPlaceHolderScreen.fxml";
 
-    private ObservableList<Person> filteredPersons;
+    private ObservableList<ReadOnlyViewablePerson> filteredPersons;
 
     public Optional<HyperBrowser> hyperBrowser;
 
-    public BrowserManager(ObservableList<Person> filteredPersons) {
+    public BrowserManager(ObservableList<ReadOnlyViewablePerson> filteredPersons) {
         this.filteredPersons = filteredPersons;
         String headlessProperty = System.getProperty("testfx.headless");
         if (headlessProperty != null && headlessProperty.equals("true")) {
@@ -73,7 +73,7 @@ public class BrowserManager {
     private synchronized void updateBrowserContent() {
         ArrayList<URL> pagesPerson = hyperBrowser.get().getCachedPagesUrl();
         pagesPerson.stream().forEach(personUrl -> {
-                Optional<Person> personFound = filteredPersons.stream().filter(person
+                Optional<ReadOnlyViewablePerson> personFound = filteredPersons.stream().filter(person
                         -> UrlUtil.compareBaseUrls(person.profilePageUrl(), personUrl)).findAny();
 
                 if (!personFound.isPresent()){
@@ -93,19 +93,18 @@ public class BrowserManager {
      * Loads the person's profile page to the browser.
      * PreCondition: filteredModelPersons.size() >= 1
      */
-    public synchronized void loadProfilePage(Person person) {
+    public synchronized void loadProfilePage(ReadOnlyViewablePerson person) {
         if (!hyperBrowser.isPresent()) return;
 
         int indexOfPersonInListOfContacts = filteredPersons.indexOf(person);
 
-        ArrayList<Person> listOfPersonToLoadInFuture = getListOfPersonToLoadInFuture(filteredPersons,
+        ArrayList<ReadOnlyViewablePerson> listOfPersonToLoadInFuture = getListOfPersonToLoadInFuture(filteredPersons,
                                                                                      indexOfPersonInListOfContacts);
         try {
             ArrayList<URL> listOfFutureUrl = listOfPersonToLoadInFuture.stream()
                                                                        .map(p -> p.profilePageUrl())
                                                                        .collect(Collectors.toCollection(ArrayList::new));
-            GithubProfilePage page = (GithubProfilePage) hyperBrowser.get().loadUrls(person.profilePageUrl(),
-                                                                                     listOfFutureUrl);
+            hyperBrowser.get().loadUrls(person.profilePageUrl(), listOfFutureUrl);
         } catch (IllegalArgumentSizeException e) {
             e.printStackTrace();
             assert false : "Will never go into here if preconditions of loadUrls is fulfilled.";
@@ -115,11 +114,11 @@ public class BrowserManager {
     /**
      * Gets a list of person that are needed to be loaded to the browser in future.
      */
-    private ArrayList<Person> getListOfPersonToLoadInFuture(List<Person> filteredPersons, int indexOfPerson) {
-        ArrayList<Person> listOfRequiredPerson = new ArrayList<>();
+    private ArrayList<ReadOnlyViewablePerson> getListOfPersonToLoadInFuture(List<ReadOnlyViewablePerson> filteredPersons, int indexOfPerson) {
+        ArrayList<ReadOnlyViewablePerson> listOfRequiredPerson = new ArrayList<>();
 
         for (int i = 1; i < HyperBrowser.NUMBER_OF_PRELOADED_PAGE && i < filteredPersons.size(); i++){
-            listOfRequiredPerson.add(new Person(filteredPersons.get((indexOfPerson + i) % filteredPersons.size())));
+            listOfRequiredPerson.add(filteredPersons.get((indexOfPerson + i) % filteredPersons.size()));
         }
         return listOfRequiredPerson;
     }

src/main/java/address/browser/page/GithubProfilePage.java
@@ -1,7 +1,6 @@
 package address.browser.page;
 
 import address.browser.EmbeddedBrowser;
-import address.model.datatypes.Person;
 
 /**
  * A github profile page

src/main/java/address/controller/MainController.java
@@ -390,7 +390,7 @@ public class MainController {
 
     public void showPersonWebPage() {
 
-        this.browserManager = new BrowserManager(modelManager.getFilteredPersons());
+        this.browserManager = new BrowserManager(modelManager.getAllViewablePersonsReadOnly());
         SplitPane pane = (SplitPane) rootLayout.lookup("#splitPane");
         pane.getItems().add(browserManager.getHyperBrowserView());
     }

src/main/java/address/model/datatypes/person/Person.java
@@ -203,7 +203,7 @@ public class Person extends BaseDataType implements ReadOnlyPerson {
     @Override
     public Optional<String> githubProfilePicUrl() {
         if (getGithubUserName().length() > 0) {
-            String profilePicUrl = githubProfilePageUrl() + ".png";
+            String profilePicUrl = profilePageUrl().toExternalForm() + ".png";
             if (URLUtil.isURIFormat(profilePicUrl)){
                 return Optional.of(profilePicUrl);
             }

src/main/java/address/model/datatypes/person/ReadOnlyPerson.java
@@ -8,6 +8,7 @@ import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.ReadOnlyStringProperty;
 import javafx.collections.ObservableList;
 
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.Optional;
 import java.util.function.BiConsumer;
@@ -29,7 +30,7 @@ public interface ReadOnlyPerson extends ExtractableObservables {
     /**
      * @return github profile url
      */
-    String githubProfilePageUrl();
+    URL profilePageUrl();
     Optional<String> githubProfilePicUrl();
 
     String getStreet();

src/main/java/address/model/datatypes/person/ViewablePerson.java
@@ -6,6 +6,8 @@ import javafx.beans.property.*;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -143,9 +145,21 @@ public class ViewablePerson extends Viewable<Person> implements ReadOnlyViewable
     }
 
     @Override
-    public String githubProfilePageUrl() {
-        return visible.githubProfilePageUrl();
+    public URL profilePageUrl() {
+        URL url = null;
+
+        try {
+            url = new URL("https://github.com/" + getGithubUserName());
+        } catch (MalformedURLException e) {
+            try {
+                url = new URL("https://github.com");
+            } catch (MalformedURLException e1) {
+                assert false;
+            }
+        }
+        return url;
     }
+
 
     @Override
     public Optional<String> githubProfilePicUrl() {