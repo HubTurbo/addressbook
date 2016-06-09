c90caed7609ef0d4fa31b19e95341ee730678e75
Damith C. Rajapakse <damith@gmail.com>
2016-06-09 18:49:27 +0800

    Added two key sequences

--- src/main/java/address/events/PotentialKeyboardShortcutEvent.java
+++ src/main/java/address/events/PotentialKeyboardShortcutEvent.java
@@ -2,6 +2,11 @@ package address.events;
 
 import javafx.scene.input.KeyEvent;
 
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
 /**
  * Indicates a key event occurred that is potentially a keyboard shortcut
  */
@@ -10,8 +15,26 @@ public class PotentialKeyboardShortcutEvent extends BaseEvent{
     /** The key event */
     public KeyEvent keyEvent;
 
+    /** The time that the Key event occurred */
+    public long time;
+
     public PotentialKeyboardShortcutEvent(KeyEvent keyEvent){
+        this.time = System.nanoTime();
         this.keyEvent = keyEvent;
+    }
+
+    /**
+     * Returns the elapsed time between the given two events.
+     * @param firstEvent
+     * @param secondEvent
+     * @return elapsed time in milli seconds.
+     */
+    public static long elapsedTimeInMilliseconds(PotentialKeyboardShortcutEvent firstEvent,
+                                                 PotentialKeyboardShortcutEvent secondEvent){
+        long durationInNanoSeconds = secondEvent.time - firstEvent.time;
+        long elapsedTimeInMilliseconds = MILLISECONDS.convert(durationInNanoSeconds, NANOSECONDS);
+        assert elapsedTimeInMilliseconds >= 0;
+        return elapsedTimeInMilliseconds;
     }
 
     @Override
---
+++ src/main/java/address/shortcuts/KeySequence.java
@@ -0,0 +1,19 @@
+package address.shortcuts;
+
+import address.events.BaseEvent;
+import javafx.scene.input.KeyCode;
+
+/**
+ * Represents a shortcut that is a key sequence e.g. G followed by P
+ */
+public class KeySequence {
+    protected KeyCode firstKey;
+    protected KeyCode secondKey;
+    protected BaseEvent eventToRaise;
+
+    public KeySequence(KeyCode firstKey, KeyCode secondKey, BaseEvent eventToRaise) {
+        this.firstKey = firstKey;
+        this.secondKey = secondKey;
+        this.eventToRaise = eventToRaise;
+    }
+}
