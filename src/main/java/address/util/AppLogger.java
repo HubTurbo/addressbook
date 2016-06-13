51d904c34d2567e5f609273c731b1c310fd943af
m133225 <m133225@hotmail.com>
2016-06-13 11:47:56 +0800

    Implemented AppLogger which wraps Logger.

---
+++ src/main/java/address/util/AppLogger.java
@@ -0,0 +1,45 @@
+package address.util;
+
+import address.events.BaseEvent;
+import org.apache.logging.log4j.Logger;
+
+public class AppLogger {
+    private Logger logger;
+
+    public AppLogger(Logger logger) {
+        this.logger = logger;
+    }
+
+    public void info(String message, Object... params) {
+        logger.info(message, params);
+    }
+
+    public void debug(String message, Object... params) {
+        logger.debug(message, params);
+    }
+
+    public void trace(String message, Object... params) {
+        logger.trace(message, params);
+    }
+
+    public void warn(String message, Object... params) {
+        logger.warn(message, params);
+
+    }
+
+    public void error(String message, Object... params) {
+        logger.error(message, params);
+    }
+
+    public void fatal(String message, Object... params) {
+        logger.fatal(message, params);
+    }
+
+    public <T extends Throwable> void throwing(T throwable) {
+        logger.throwing(throwable);
+    }
+
+    public void logEvent(BaseEvent event) {
+        logger.info("{}: {}", event.getClass().getName(), event.toString());
+    }
+}
