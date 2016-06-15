2ff8963de3f80a1801c64ce60498208872d38dc3
You Liang <a0125975@u.nus.edu>
2016-06-15 15:30:44 +0800

    Fix wrong type

--- src/test/java/address/unittests/browser/BrowserManagerTest.java
+++ src/test/java/address/unittests/browser/BrowserManagerTest.java
@@ -1,12 +1,11 @@
 package address.unittests.browser;
 
 import address.browser.BrowserManager;
+import javafx.collections.FXCollections;
 import org.junit.Test;
 
-import java.util.Collections;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 
 /**
  * Tests the BrowserManager behaviours and functionality.
@@ -15,7 +14,7 @@ public class BrowserManagerTest {
 
     @Test
     public void testNecessaryBrowserResources_resourcesFound() {
-        BrowserManager manager = new BrowserManager(Collections.emptyList());
+        BrowserManager manager = new BrowserManager(FXCollections.observableArrayList());
         assertNotNull(manager.getHyperBrowserView());
     }
 
