a196062f67ec20f57e8fa60d8273351d2bdfc251
You Liang <a0125975@u.nus.edu>
2016-06-22 10:14:18 +0800

    Fix for enum browser type

--- src/test/java/address/unittests/browser/HyperBrowserTest.java
+++ src/test/java/address/unittests/browser/HyperBrowserTest.java
@@ -42,14 +42,14 @@ public class HyperBrowserTest {
 
     @Test
     public void testFullFeatureBrowser_loadUrl_urlAssigned() throws MalformedURLException, InterruptedException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.FULL_FEATURE_BROWSER, 1, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.FULL_FEATURE_BROWSER, 1, Optional.empty());
         Page page = browser.loadUrl(new URL("https://github.com"));
         assertTrue(UrlUtil.compareBaseUrls(page.getBrowser().getOriginUrl(), new URL("https://github.com")));
     }
 
     @Test
     public void testFullFeatureBrowser_loadUrls_urlsAssigned() throws MalformedURLException, IllegalAccessException, NoSuchFieldException, InterruptedException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.FULL_FEATURE_BROWSER, 3, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.FULL_FEATURE_BROWSER, 3, Optional.empty());
 
         List<URL> listOfUrl = Arrays.asList(new URL("https://github.com"), new URL("https://google.com.sg"), new URL("https://sg.yahoo.com"));
         Page page = browser.loadUrls(listOfUrl.get(0), listOfUrl.subList(1,3));
@@ -67,7 +67,7 @@ public class HyperBrowserTest {
 
     @Test
     public void testFullFeatureBrowser_loadUrlsMultipleTimesNoOverLapping_correctUrlsAssigned() throws MalformedURLException, IllegalAccessException, NoSuchFieldException, InterruptedException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.FULL_FEATURE_BROWSER, 3, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.FULL_FEATURE_BROWSER, 3, Optional.empty());
 
 
         browser.loadUrls(listOfUrl.get(0), listOfUrl.subList(1,3));
@@ -84,7 +84,7 @@ public class HyperBrowserTest {
 
     @Test
     public void testFullFeatureBrowser_loadUrlsMultipleTimesOverLapping_correctUrlsAssigned() throws MalformedURLException, IllegalAccessException, NoSuchFieldException, InterruptedException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.FULL_FEATURE_BROWSER, 3, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.FULL_FEATURE_BROWSER, 3, Optional.empty());
         browser.loadUrls(listOfUrl.get(0), listOfUrl.subList(1,3));
         TimeUnit.SECONDS.sleep(3);
         Page page = browser.loadUrls(listOfUrl.get(2), listOfUrl.subList(3,5));
@@ -100,7 +100,7 @@ public class HyperBrowserTest {
 
     @Test
     public void testFullFeatureBrowser_loadUrlsMultipleTimesWithDuplicates_correctUrlsAssigned() throws MalformedURLException, IllegalAccessException, NoSuchFieldException, InterruptedException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.FULL_FEATURE_BROWSER, 3, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.FULL_FEATURE_BROWSER, 3, Optional.empty());
         browser.loadUrls(listOfUrl.get(0), listOfUrl.subList(1,3));
         TimeUnit.SECONDS.sleep(3);
         Page page = browser.loadUrls(listOfUrl.get(2), Arrays.asList(listOfUrl.get(0), listOfUrl.get(0)));
@@ -115,7 +115,7 @@ public class HyperBrowserTest {
 
     @Test
     public void testFullFeatureBrowser_loadUrlsThenLoadUrl_displayedUrlRemoved() throws MalformedURLException, IllegalAccessException, NoSuchFieldException, InterruptedException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.FULL_FEATURE_BROWSER, 3, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.FULL_FEATURE_BROWSER, 3, Optional.empty());
         browser.loadUrls(listOfUrl.get(0), listOfUrl.subList(1,3));
         TimeUnit.SECONDS.sleep(3);
         Page page = browser.loadUrl(listOfUrl.get(4));
@@ -131,7 +131,7 @@ public class HyperBrowserTest {
 
     @Test
     public void testLimitedFeatureBrowser_loadUrl_urlAssigned() throws MalformedURLException, InterruptedException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.LIMITED_FEATURE_BROWSER, 1, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.LIMITED_FEATURE_BROWSER, 1, Optional.empty());
         URL url = new URL("https://github.com");
         Page page = browser.loadUrl(url);
         assertTrue(UrlUtil.compareBaseUrls(page.getBrowser().getOriginUrl(), url));
@@ -139,7 +139,7 @@ public class HyperBrowserTest {
 
     @Test
     public void testLimitedFeatureBrowser_loadUrls_urlsAssigned() throws MalformedURLException, IllegalAccessException, NoSuchFieldException, InterruptedException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.LIMITED_FEATURE_BROWSER, 3, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.LIMITED_FEATURE_BROWSER, 3, Optional.empty());
         Page page = browser.loadUrls(listOfUrl.get(0), listOfUrl.subList(1,3));
         assertTrue(UrlUtil.compareBaseUrls(page.getBrowser().getOriginUrl(), listOfUrl.get(0)));
 
@@ -155,7 +155,7 @@ public class HyperBrowserTest {
 
     @Test
     public void testClearPage_removeOnePage_pageRemoved() throws MalformedURLException, IllegalAccessException, NoSuchFieldException, InterruptedException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.FULL_FEATURE_BROWSER, 3, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.FULL_FEATURE_BROWSER, 3, Optional.empty());
         Page page = browser.loadUrls(listOfUrl.get(0), listOfUrl.subList(1,3));
         browser.clearPage(page.getBrowser().getOriginUrl());
         Field pages = browser.getClass().getDeclaredField("pages");
@@ -168,7 +168,7 @@ public class HyperBrowserTest {
 
     @Test
     public void testGetDisplayedUrl_loadOnePage_getDisplayedUrlReturnPageUrl() throws MalformedURLException {
-        HyperBrowser browser = new HyperBrowser(HyperBrowser.FULL_FEATURE_BROWSER, 1, Optional.empty());
+        HyperBrowser browser = new HyperBrowser(HyperBrowser.Type.FULL_FEATURE_BROWSER, 1, Optional.empty());
         Page page = browser.loadUrl(new URL("https://github.com"));
         assertTrue(UrlUtil.compareBaseUrls(browser.getDisplayedUrl(), new URL("https://github.com")));
     }
