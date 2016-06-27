613d28a3710d59603e0e56e64cc84759f66f7442
m133225 <m133225@hotmail.com>
2016-06-27 15:46:04 +0800

    Extended test.

--- src/test/java/address/guitests/FullSystemTest.java
+++ src/test/java/address/guitests/FullSystemTest.java
@@ -11,33 +11,39 @@ public class FullSystemTest extends GuiTestBase {
     public void scenarioOne() {
         clickOn("Tags").clickOn("New Tag")
                 .clickOn("#tagNameField").write("colleagues").type(KeyCode.ENTER)
-                .clickOn("Muster").type(KeyCode.E).clickOn("#firstNameField").eraseText(4).write("John")
+                .clickOn("Muster").type(KeyCode.E).clickOn("#firstNameField").push(shortcut(KeyCode.A)).eraseText(4).write("John")
                 .clickOn("#lastNameField").eraseText(6).write("Tan")
                 .clickOn("#cityField").write("Singapore")
                 .clickOn("#githubUserNameField").write("john123")
                 .clickOn("#tagList").write("coll").type(KeyCode.SPACE)
-                .type(KeyCode.ENTER).sleep(200)//wait for closing animation
+                .type(KeyCode.ENTER).sleep(2000)//wait for closing animation
                 .type(KeyCode.ENTER)
-        .clickOn("#filterField").write("tag:colleagues").type(KeyCode.ENTER)
-        .clickOn("John").type(KeyCode.D).sleep(1000)
+        //.clickOn("#filterField").write("tag:colleagues").type(KeyCode.ENTER)
+//                .sleep(1000)
+        .clickOn("John")
+                .type(KeyCode.D)
+                .sleep(1000)
                 .clickOn("Tags").clickOn("Manage Tags").rightClickOn("colleagues").clickOn("New").write("company")
                 .type(KeyCode.ENTER).type(KeyCode.ESCAPE)
         .clickOn("#filterField").push(shortcut(KeyCode.A)).eraseText(1).type(KeyCode.ENTER)
-        .clickOn("Cornelia").type(KeyCode.E).clickOn("#streetField").write("My Street")
-                .clickOn("#githubUserNameField").write("cornelia321")
+        .clickOn("Ruth").type(KeyCode.E).clickOn("#streetField").write("My Street")
+                .clickOn("#githubUserNameField").write("ruth321")
                 .type(KeyCode.ENTER)
         .clickOn("#filterField").write("name:Mueller").type(KeyCode.ENTER)
                 .clickOn("Martin").type(KeyCode.E).clickOn("#tagList").write("frien").type(KeyCode.SPACE)
                 .type(KeyCode.ENTER).sleep(200)
                 .type(KeyCode.ENTER)
         .clickOn("Help").clickOn("About").clickOn("OK")
+
+        .clickOn("New").clickOn("#firstNameField").write("Ming").clickOn("OK").targetWindow("Invalid Fields").clickOn("OK")
+                .clickOn("#lastNameField").write("Lee").clickOn("OK")
         .clickOn("File").clickOn("[Local] Save As...").sleep(1000).type(KeyCode.S).type(KeyCode.ENTER)
         .clickOn("File").clickOn("[Local] Save").clickOn("File").clickOn("Exit");
     }
 
     @After
     public void cleanup() {
-        File file = new File("s.xml");
+        File file = new File("src/test/data/sandbox/s.xml");
         file.delete();
     }
 }
