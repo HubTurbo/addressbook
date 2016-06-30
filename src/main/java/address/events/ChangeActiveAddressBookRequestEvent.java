81e2854790904a24597c4bb235dd78f1caa60877
Martin Choo <m133225@users.noreply.github.com>
2016-06-30 20:13:18 +0800

    [353] Create an Event for model to set the active addressbook on SyncManager

---
+++ src/main/java/address/events/ChangeActiveAddressBookRequestEvent.java
@@ -0,0 +1,21 @@
+package address.events;
+
+/**
+ * Event to request for changing of active address book
+ */
+public class ChangeActiveAddressBookRequestEvent extends BaseEvent {
+    private String activeAddressBookName;
+
+    public ChangeActiveAddressBookRequestEvent(String activeAddressBookName) {
+        this.activeAddressBookName = activeAddressBookName;
+    }
+
+    public String getActiveAddressBookName() {
+        return activeAddressBookName;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
