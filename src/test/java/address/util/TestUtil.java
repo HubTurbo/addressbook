312fd068290ed26bca57173746fa2fdea202498f
Damith C. Rajapakse <damith@gmail.com>
2016-06-30 20:20:05 +0800

    [293] A test in KeyBindingsManagerApiTest fails locally
    * Unified two solutions to the SHORTCUT problem on Mac
    
    * Tweaked platform specific shortcut key to depend on headful/headless mode. Added test to tag edit dialog to verify behaviour of shortcut + a.

--- src/test/java/address/util/TestUtil.java
+++ src/test/java/address/util/TestUtil.java
@@ -2,13 +2,17 @@ package address.util;
 
 import address.TestApp;
 import address.model.datatypes.AddressBook;
-import address.model.datatypes.ReadOnlyAddressBook;
 import address.model.datatypes.person.Person;
 import address.model.datatypes.person.ViewablePerson;
 import address.model.datatypes.tag.Tag;
 import address.storage.StorageAddressBook;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,7 +21,6 @@ import java.util.stream.Collectors;
  * A utility class for test cases.
  */
 public class TestUtil {
-
     /**
      * Folder used for temp files created during testing. Ignored by Git.
      */
@@ -71,7 +74,6 @@ public class TestUtil {
     }
 
     public static void main(String... s) {
-        StorageAddressBook sa = generateSampleStorageAddressBook();
         createDataFileWithSampleData(TestApp.SAVE_LOCATION_FOR_TESTING);
     }
 
@@ -81,5 +83,74 @@ public class TestUtil {
 
     public static StorageAddressBook generateSampleStorageAddressBook() {
         return new StorageAddressBook(generateSampleAddressBook());
+    }
+
+    /**
+     * Tweaks the {@code keyCodeCombination} to resolve the {@code KeyCode.SHORTCUT} to their
+     * respective platform-specific keycodes
+     */
+    public static KeyCode[] scrub(KeyCodeCombination keyCodeCombination) {
+        List<KeyCode> keys = new ArrayList<>();
+        if (keyCodeCombination.getAlt() == KeyCombination.ModifierValue.DOWN) {
+            keys.add(KeyCode.ALT);
+        }
+        if (keyCodeCombination.getShift() == KeyCombination.ModifierValue.DOWN) {
+            keys.add(KeyCode.SHIFT);
+        }
+        if (keyCodeCombination.getMeta() == KeyCombination.ModifierValue.DOWN) {
+            keys.add(KeyCode.META);
+        }
+        if (keyCodeCombination.getControl() == KeyCombination.ModifierValue.DOWN) {
+            keys.add(KeyCode.CONTROL);
+        }
+        if (keyCodeCombination.getShortcut() == KeyCombination.ModifierValue.DOWN) {
+            keys.add(getPlatformSpecificShortcutKey());
+
+        }
+        keys.add(keyCodeCombination.getCode());
+        return keys.toArray(new KeyCode[]{});
+    }
+
+    public static boolean isHeadlessEnvironment() {
+        String headlessProperty = System.getProperty("testfx.headless");
+        return headlessProperty != null && headlessProperty.equals("true");
+    }
+
+    /**
+     * Returns {@code KeyCode.COMMAND or KeyCode.META} if on a Mac depending on headful/headless mode,
+     * {@code KeyCode.CONTROL} otherwise.
+     */
+    private static KeyCode getPlatformSpecificShortcutKey() {
+        KeyCode macShortcut = isHeadlessEnvironment() ? KeyCode.COMMAND : KeyCode.META;
+        return OsDetector.isOnMac() ? macShortcut : KeyCode.CONTROL;
+    }
+
+    /**
+     * Replaces any {@code KeyCode.SHORTCUT or KeyCode.META} with {@code KeyCode.META or KeyCode.COMMAND} on Macs
+     * depending on headful/headless mode, and {@code KeyCode.CONTROL} on other platforms.
+     */
+    public static KeyCode[] scrub(KeyCode[] keyCodes) {
+        for (int i = 0; i < keyCodes.length; i++) {
+            if (keyCodes[i] == KeyCode.META || keyCodes[i] == KeyCode.SHORTCUT) {
+                keyCodes[i] = getPlatformSpecificShortcutKey();
+            }
+        }
+        return keyCodes;
+    }
+
+    /**
+     * Generates a minimal {@link KeyEvent} object that matches the {@code keyCombination}
+     */
+    public static KeyEvent getKeyEvent(String keyCombination) {
+        String[] keys = keyCombination.split(" ");
+
+        String key = keys[keys.length - 1];
+        boolean shiftDown = keyCombination.toLowerCase().contains("shift");
+        boolean metaDown = keyCombination.toLowerCase().contains("meta")
+                || (keyCombination.toLowerCase().contains("shortcut") && OsDetector.isOnMac());
+        boolean altDown = keyCombination.toLowerCase().contains("alt");
+        boolean ctrlDown = keyCombination.toLowerCase().contains("ctrl")
+                || keyCombination.toLowerCase().contains("shortcut") && !OsDetector.isOnMac();
+        return new KeyEvent(null, null, null, KeyCode.valueOf(key), shiftDown, ctrlDown, altDown, metaDown);
     }
 }
