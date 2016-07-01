HubTurbo/addressbook

commit 10c371fa5d6d3b30e50a913c5dedc1bf3ebfbc00

[311] Initialize save location in the footer status bar
* remove binding as it causes some stackoverflow problem. use simple initialization

* Add secondaryLabel after updater has completed its job

* remove defensive codes. Since updaterevent won't get called twice

* Remove useless imports. StringProperty doesn't seems to work.

* Refactor: extract method for common codes

* Refactor: renaming methods and create new sub methods

* Refactor: change method input parameter type to be more specific. remove duplicated codes.

src/main/java/address/controller/MainController.java
@@ -183,7 +183,7 @@ public class MainController extends UiController{
             GridPane gridPane = loader.load();
             gridPane.getStyleClass().add("grid-pane");
             StatusBarFooterController controller = loader.getController();
-            controller.initStatusBar(config);
+            controller.init(config.updateInterval, modelManager.getPrefs().getSaveLocationString());
             rootLayout.getChildren().add(gridPane);
         } catch (IOException e) {
             logger.warn("Error Loading footer status bar: {}", e);

src/main/java/address/controller/StatusBarFooterController.java
@@ -32,15 +32,15 @@ public class StatusBarFooterController extends UiController{
     private TickingTimer timer;
     private long updateIntervalInSecs;
 
-    private final Label saveLocationLabel;
+    private final Label secondaryStatusBarLabel;
 
     public StatusBarFooterController() {
         super();
-        this.saveLocationLabel = new Label("");
+        this.secondaryStatusBarLabel = new Label("");
     }
 
-    private void updateWithConfigValues(Config config) {
-        updateIntervalInSecs = (int) DateTimeUtil.millisecsToSecs(config.updateInterval);
+    private void initSyncTimer(long updateInterval) {
+        updateIntervalInSecs = (int) DateTimeUtil.millisecsToSecs(updateInterval);
         timer = new TickingTimer("Sync timer", (int) updateIntervalInSecs,
                 this::syncSchedulerOntick, this::syncSchedulerOnTimeOut, TimeUnit.SECONDS);
     }
@@ -57,27 +57,28 @@ public class StatusBarFooterController extends UiController{
 
     /**
      * Initializes the status bar
-     *
-     * The given config object should have set updatedInterval
-     *
-     * @param config
+     * @param updateInterval The sync period
+     * @param saveLocation The location to save the file for storing Addressbook contacts.
      */
-    public void initStatusBar(Config config) {
-        updateWithConfigValues(config);
+    public void init(long updateInterval, String saveLocation) {
+        initSyncTimer(updateInterval);
+        initStatusBar();
+        initSaveLocationLabel(saveLocation);
+    }
+
+    private void initSaveLocationLabel(String saveLocation) {
+        updateSaveLocationDisplay(saveLocation);
+        secondaryStatusBarLabel.setTextAlignment(TextAlignment.LEFT);
+        setTooltip(secondaryStatusBarLabel);
+    }
+
+    private void initStatusBar() {
         this.syncStatusBar = new StatusBar();
         this.updaterStatusBar = new StatusBar();
         FxViewUtil.applyAnchorBoundaryParameters(syncStatusBar, 0.0, 0.0, 0.0, 0.0);
         FxViewUtil.applyAnchorBoundaryParameters(updaterStatusBar, 0.0, 0.0, 0.0, 0.0);
         syncStatusBarPane.getChildren().add(syncStatusBar);
         updaterStatusBarPane.getChildren().add(updaterStatusBar);
-        initSaveLocationLabel();
-    }
-
-    private void initSaveLocationLabel() {
-        saveLocationLabel.setTextAlignment(TextAlignment.LEFT);
-        setTooltip(saveLocationLabel);
-        this.updaterStatusBar.getRightItems().add(saveLocationLabel);
-        saveLocationLabel.setVisible(false);
     }
 
     private void setTooltip(Label label) {
@@ -131,8 +132,13 @@ public class StatusBarFooterController extends UiController{
             updaterStatusBar.setText(ufe.toString());
             updaterStatusBar.setProgress(0.0);
             updaterStatusBar.setText("");
-            saveLocationLabel.setVisible(true);
+            showSecondaryStatusBarLabel();
         });
+    }
+
+    private void showSecondaryStatusBarLabel() {
+        secondaryStatusBarLabel.setVisible(true);
+        this.updaterStatusBar.getRightItems().add(secondaryStatusBarLabel);
     }
 
     @Subscribe
@@ -141,16 +147,16 @@ public class StatusBarFooterController extends UiController{
             updaterStatusBar.setText(ufe.toString());
             updaterStatusBar.setProgress(0.0);
             updaterStatusBar.setText("");
-            saveLocationLabel.setVisible(true);
+            showSecondaryStatusBarLabel();
         });
     }
 
     @Subscribe
     private void handleSaveLocationChangedEvent(SaveLocationChangedEvent e) {
-        updateSaveLocationDisplay(e.saveFile);
+        updateSaveLocationDisplay(e.saveFile.getName());
     }
 
-    private void updateSaveLocationDisplay(File saveFile) {
-        saveLocationLabel.setText(SAVE_LOC_TEXT_PREFIX + ((saveFile != null) ? saveFile.getName() : LOC_TEXT_NOT_SET));
+    private void updateSaveLocationDisplay(String saveFile) {
+        secondaryStatusBarLabel.setText(SAVE_LOC_TEXT_PREFIX + ((saveFile != null) ? saveFile : LOC_TEXT_NOT_SET));
     }
 }

src/main/java/address/model/UserPrefs.java
@@ -1,8 +1,5 @@
 package address.model;
 
-import address.util.AppLogger;
-import address.util.LoggerManager;
-
 import java.io.File;
 
 /**
@@ -25,6 +22,10 @@ public class UserPrefs {
      */
     public synchronized File getSaveLocation() {
         return saveLocation == null ? new File(DEFAULT_TEMP_FILE_PATH) : new File(saveLocation);
+    }
+
+    public String getSaveLocationString() {
+        return saveLocation;
     }
 
     public boolean isSaveLocationSet() {

commit 00da250df77835fd83c16ff464bfcd138439d263

Fix regression caused by PR #341

src/main/java/address/controller/MainController.java
@@ -183,7 +183,7 @@ public class MainController extends UiController{
             GridPane gridPane = loader.load();
             gridPane.getStyleClass().add("grid-pane");
             StatusBarFooterController controller = loader.getController();
-            controller.init(config.updateInterval, modelManager.getPrefs().getSaveLocationString());
+            controller.init(config.updateInterval, modelManager.getPrefs().getSaveLocation());
             rootLayout.getChildren().add(gridPane);
         } catch (IOException e) {
             logger.warn("Error Loading footer status bar: {}", e);

src/main/java/address/controller/StatusBarFooterController.java
@@ -60,13 +60,13 @@ public class StatusBarFooterController extends UiController{
      * @param updateInterval The sync period
      * @param saveLocation The location to save the file for storing Addressbook contacts.
      */
-    public void init(long updateInterval, String saveLocation) {
+    public void init(long updateInterval, File saveLocation) {
         initSyncTimer(updateInterval);
         initStatusBar();
         initSaveLocationLabel(saveLocation);
     }
 
-    private void initSaveLocationLabel(String saveLocation) {
+    private void initSaveLocationLabel(File saveLocation) {
         updateSaveLocationDisplay(saveLocation);
         secondaryStatusBarLabel.setTextAlignment(TextAlignment.LEFT);
         setTooltip(secondaryStatusBarLabel);
@@ -153,10 +153,10 @@ public class StatusBarFooterController extends UiController{
 
     @Subscribe
     private void handleSaveLocationChangedEvent(SaveLocationChangedEvent e) {
-        updateSaveLocationDisplay(e.saveFile.getName());
+            updateSaveLocationDisplay(e.saveFile);
     }
 
-    private void updateSaveLocationDisplay(String saveFile) {
-        secondaryStatusBarLabel.setText(SAVE_LOC_TEXT_PREFIX + ((saveFile != null) ? saveFile : LOC_TEXT_NOT_SET));
+    private void updateSaveLocationDisplay(File saveFile) {
+        secondaryStatusBarLabel.setText(SAVE_LOC_TEXT_PREFIX + ((saveFile != null) ? saveFile.getName() : LOC_TEXT_NOT_SET));
     }
 }