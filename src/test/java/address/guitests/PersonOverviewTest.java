3a9c956a145f2ab841de6babf4ab2d629de915c4
Martin Choo <m133225@users.noreply.github.com>
2016-07-01 17:41:41 +0800

    [370] Add gui tests to verify behaviour of drag and drop

---
+++ src/test/java/address/guitests/PersonOverviewTest.java
@@ -0,0 +1,24 @@
+package address.guitests;
+
+import javafx.scene.control.Label;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PersonOverviewTest extends GuiTestBase {
+    @Test
+    public void dragAndDrop_firstToSecond() {
+        Label hansNameLabel = getNameLabelOf("Hans");
+        Label ruthIdLabel = getNameLabelOf("Ruth");
+        assertTrue(hansNameLabel.localToScreen(0, 0).getY() < ruthIdLabel.localToScreen(0, 0).getY());
+        drag("Hans").dropTo("Heinz");// drag from first to start of 3rd (slightly further down between 2nd and 3rd)
+
+        Label hansNameLabel2 = getNameLabelOf("Hans");
+        Label ruthIdLabel2 = getNameLabelOf("Ruth");
+        assertTrue(hansNameLabel2.localToScreen(0, 0).getY() > ruthIdLabel2.localToScreen(0, 0).getY());
+    }
+
+    private Label getNameLabelOf(String name) {
+        return (Label) lookup(name).tryQuery().get();
+    }
+}
