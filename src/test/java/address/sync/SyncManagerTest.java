ed1bccc61200f052b8f7c35d3071f32dd769acd0
Martin Choo <m133225@users.noreply.github.com>
2016-07-05 13:42:02 +0800

    [409] PowerMock class loader conflicts with frameworks
    * Added ignore annotation to push class loading to framework's class loader.
    
    * Added comments for PowerMockIgnore.

--- src/test/java/address/sync/SyncManagerTest.java
+++ src/test/java/address/sync/SyncManagerTest.java
@@ -12,7 +12,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -28,6 +28,7 @@ import static org.powermock.api.mockito.PowerMockito.whenNew;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({CreatePersonOnRemoteTask.class, SyncManager.class})
+@PowerMockIgnore({"javax.management.*"})// Defer loading of javax.management.* in log4j to system class loader
 public class SyncManagerTest {
     RemoteManager remoteManager;
     SyncManager syncManager;
