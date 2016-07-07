deb53eeb4d7cc5ec0ef426b0369a7838078336fe
You Liang <youliang@u.nus.edu>
2016-07-07 16:08:57 +0800

    [435] Remove saveLocation from UserPrefs.

--- src/main/java/address/model/UserPrefs.java
+++ src/main/java/address/model/UserPrefs.java
@@ -2,44 +2,12 @@ package address.model;
 
 import address.util.GuiSettings;
 
-import java.io.File;
-
 /**
  * Represents User's preferences.
  */
 public class UserPrefs {
 
-    private static final String DEFAULT_TEMP_FILE_PATH = ".$TEMP_ADDRESS_BOOK";
-
-    /**
-     * Full path (including file name) of the data file to be used for local storage
-     */
-    private volatile String saveLocation;
-
     public GuiSettings guiSettings;
-
-    public synchronized void setSaveLocation(String saveLocation) {
-        this.saveLocation = saveLocation;
-    }
-
-    /**
-     * @return the current save file location or the default temp file location if there is no recorded preference.
-     */
-    public synchronized File getSaveLocation() {
-        return saveLocation == null ? new File(DEFAULT_TEMP_FILE_PATH) : new File(saveLocation);
-    }
-
-    public String getSaveFileName() {
-        return getSaveLocation().getName();
-    }
-
-    public String getSaveLocationString() {
-        return saveLocation;
-    }
-
-    public boolean isSaveLocationSet() {
-        return getSaveLocation() != null;
-    }
 
     public GuiSettings getGuiSettings() {
         return guiSettings == null ? new GuiSettings() : guiSettings;
