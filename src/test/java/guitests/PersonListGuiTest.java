dc15fde22a411926904bd2f3dbd05b3b3a29ab3a
Damith C. Rajapakse <damith@gmail.com>
2016-07-09 18:07:11 +0800

    Converted test data to static

--- src/test/java/guitests/PersonListGuiTest.java
+++ src/test/java/guitests/PersonListGuiTest.java
@@ -6,6 +6,7 @@ import address.model.datatypes.person.Person;
 import address.sync.cloud.model.CloudAddressBook;
 import address.util.TestUtil;
 import org.junit.Test;
+import static guitests.PersonListGuiTest.TypicalData.*;
 
 import static org.junit.Assert.assertTrue;
 
@@ -13,23 +14,23 @@ public class PersonListGuiTest extends GuiTestBase {
 
     private AddressBook initialData = generateInitialData();
 
+
     private AddressBook generateInitialData() {//TODO: create a better set of sample data
         AddressBook ab = new AddressBook();
-        TypicalData typicalData = new TypicalData();
-        ab.addPerson(typicalData.ALICE);
-        ab.addPerson(typicalData.BENSON);
-        ab.addPerson(typicalData.CHARLIE);
-        ab.addPerson(typicalData.DAN);
-        ab.addPerson(typicalData.ELIZABETH);
+        ab.addPerson(ALICE);
+        ab.addPerson(BENSON);
+        ab.addPerson(CHARLIE);
+        ab.addPerson(DAN);
+        ab.addPerson(ELIZABETH);
         return ab;
     }
 
-    class TypicalData{
-        Person ALICE = new Person("Alice", "Brown", 1);
-        Person BENSON = new Person("Benson", "Christopher Dean", 2);
-        Person CHARLIE =new Person("Charlie", "Davidson", 3);
-        Person DAN = new Person("Dan", "Edwards", 4);
-        Person ELIZABETH = new Person("Elizabeth", "F. Green", 5);
+    static class TypicalData{
+        static Person ALICE = new Person("Alice", "Brown", 1);
+        static Person BENSON = new Person("Benson", "Christopher Dean", 2);
+        static Person CHARLIE = new Person("Charlie", "Davidson", 3);
+        static Person DAN = new Person("Dan", "Edwards", 4);
+        static Person ELIZABETH = new Person("Elizabeth", "F. Green", 5);
     }
 
     @Override
@@ -46,21 +47,17 @@ public class PersonListGuiTest extends GuiTestBase {
     
     @Test
     public void dragAndDrop_singlePersonCorrectDrag_listReordered() {
-        TypicalData typicalData = new TypicalData();
-        Person alice = typicalData.ALICE;
-        Person benson = typicalData.BENSON;
-        Person charlie = typicalData.CHARLIE;
-        Person dan = typicalData.DAN;
-        Person elizabeth = typicalData.ELIZABETH;
-        assertTrue(personListPanel.containsInOrder(alice, benson, charlie, dan, elizabeth));
+
+        assertTrue(personListPanel.containsInOrder(ALICE, BENSON, CHARLIE, DAN, ELIZABETH));
 
         // drag first person (Alice) and drop on Charles
-        personListPanel.dragAndDrop(alice.getFirstName(), charlie.getFirstName());
-        assertTrue(personListPanel.containsInOrder(benson, alice, charlie, dan, elizabeth));
+        personListPanel.dragAndDrop(ALICE.getFirstName(), CHARLIE.getFirstName());
+        assertTrue(personListPanel.containsInOrder(BENSON, ALICE, CHARLIE, DAN, ELIZABETH));
 
         //drag last person and drop at the beginning
-        personListPanel.dragAndDrop(elizabeth.getFirstName(), benson.getFirstName());
-        assertTrue(personListPanel.containsInOrder(elizabeth, benson, alice, charlie, dan));
+        personListPanel.dragAndDrop(ELIZABETH.getFirstName(), BENSON.getFirstName());
+        assertTrue(personListPanel.containsInOrder(ELIZABETH, BENSON, ALICE, CHARLIE, DAN));
     }
+
 
 }
