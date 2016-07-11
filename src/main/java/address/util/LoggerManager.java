f1bf643473a7d9c4d7f93f134053cfe3410a5f65
Martin Choo <m133225@users.noreply.github.com>
2016-07-11 16:28:04 +0800

    [473] Logging levels for initialized loggers are not updated after reading config
    * LoggerManager now updates existing loggers upon config read
    
    * Some refactoring in LoggerManager.

--- src/main/java/address/util/LoggerManager.java
+++ src/main/java/address/util/LoggerManager.java
@@ -10,52 +10,71 @@ import java.util.HashMap;
 
 public class LoggerManager {
     private static final AppLogger logger = LoggerManager.getLogger(LoggerManager.class);
-    public static Level currentLogLevel = Level.INFO;
-    public static HashMap<String, Level> specialLogLevel = new HashMap<>();
+    private static Level currentLogLevel = Level.INFO;
+    private static HashMap<String, Level> specialLogLevels = new HashMap<>();
 
     public static void init(Config config) {
         logger.info("currentLogLevel: {}", config.currentLogLevel);
         logger.info("specialLogLevels: {}", config.specialLogLevels);
         currentLogLevel = config.currentLogLevel;
-        specialLogLevel = config.specialLogLevels;
+        specialLogLevels = config.specialLogLevels;
+
+        LoggerContext loggerContext = getLoggerContext();
+        AbstractConfiguration loggersConfig = getLoggersConfig(loggerContext);
+        updateExistingLoggersLevel(loggersConfig);
+        loggerContext.updateLoggers(loggersConfig);
     }
 
     public static AppLogger getLogger(String className, Level loggingLevel) {
-        LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false);
-        AbstractConfiguration config = (AbstractConfiguration) loggerContext.getConfiguration();
-        setLoggingLevel(config, className, loggingLevel);
-        loggerContext.updateLoggers(config);
+        setClassLoggingLevel(className, loggingLevel);
         return new AppLogger(LogManager.getLogger(className));
-    }
-
-    private static Level determineLoggingLevelToSet(String className) {
-        if (specialLogLevel != null && specialLogLevel.containsKey(className)) {
-            return specialLogLevel.get(className);
-        }
-        return currentLogLevel;
-    }
-
-    private static void setLoggingLevel(AbstractConfiguration config, String className, Level loggingLevel) {
-        if (config.getLogger(className) != null) {
-            config.getLoggerConfig(className).setLevel(loggingLevel);
-            return;
-        }
-
-        config.addLogger(className, new LoggerConfig(className, loggingLevel, true));
     }
 
     public static AppLogger getLogger(String className) {
         Level loggingLevelToSet = determineLoggingLevelToSet(className);
-
-        LoggerContext loggerContext = (LoggerContext) LogManager.getContext(false);
-        AbstractConfiguration config = (AbstractConfiguration) loggerContext.getConfiguration();
-        setLoggingLevel(config, className, loggingLevelToSet);
-        loggerContext.updateLoggers();
+        setClassLoggingLevel(className, loggingLevelToSet);
         return new AppLogger(LogManager.getLogger(className));
     }
 
     public static <T> AppLogger getLogger(Class<T> clazz) {
         if (clazz == null) return new AppLogger(LogManager.getRootLogger());
         return getLogger(clazz.getSimpleName());
+    }
+
+    private static LoggerContext getLoggerContext() {
+        return (LoggerContext) LogManager.getContext(false);
+    }
+
+    private static AbstractConfiguration getLoggersConfig(LoggerContext loggerContext) {
+        return (AbstractConfiguration) loggerContext.getConfiguration();
+    }
+
+    private static void updateExistingLoggersLevel(AbstractConfiguration absConfig) {
+        absConfig.getLoggers().forEach((loggerName, loggerConfig) -> {
+                loggerConfig.setLevel(determineLoggingLevelToSet(loggerName));
+        });
+    }
+
+    private static void setClassLoggingLevel(String className, Level loggingLevel) {
+        LoggerContext loggerContext = getLoggerContext();
+        AbstractConfiguration loggersConfig = getLoggersConfig(loggerContext);
+        setLoggingLevel(loggersConfig, className, loggingLevel);
+        loggerContext.updateLoggers(loggersConfig);
+    }
+
+    private static Level determineLoggingLevelToSet(String className) {
+        if (specialLogLevels != null && specialLogLevels.containsKey(className)) {
+            return specialLogLevels.get(className);
+        }
+        return currentLogLevel;
+    }
+
+    private static void setLoggingLevel(AbstractConfiguration config, String className, Level loggingLevel) {
+        if (config.getLogger(className) != null) {
+            config.getLogger(className).setLevel(loggingLevel);
+            return;
+        }
+
+        config.addLogger(className, new LoggerConfig(className, loggingLevel, true));
     }
 }
