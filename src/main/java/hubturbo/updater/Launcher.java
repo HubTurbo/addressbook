c4590f52b2026b29b8dd9e5e21564545589b8fc4
m133225 <m133225@hotmail.com>
2016-07-14 14:03:12 +0800

    Installer now stops running if the existing version detected is not the same.

--- src/main/java/hubturbo/updater/Launcher.java
+++ src/main/java/hubturbo/updater/Launcher.java
@@ -1,5 +1,9 @@
 package hubturbo.updater;
 
+import address.updater.VersionData;
+import address.util.FileUtil;
+import address.util.JsonUtil;
+import address.util.Version;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
@@ -13,6 +17,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -27,6 +32,8 @@ public class Launcher extends Application {
     private static final String ERROR_INSTALL = "Failed to install";
     private static final String ERROR_RUNNING = "Failed to run application";
     private static final String ERROR_TRY_AGAIN = "Please try again, or contact developer if it keeps failing.";
+    private static final String VERSION_DATA_RESOURCE = "/VersionData.json";
+    private static final String VERSION_DATA = "/VersionData.json";
 
     private final ExecutorService pool = Executors.newSingleThreadExecutor();
     private ProgressBar progressBar;
@@ -45,22 +52,63 @@ public class Launcher extends Application {
     }
 
     private void run() throws IOException {
-        Installer installer = new Installer();
+        if (shouldRunInstaller()) runInstaller();
+        runMainApplication();
+        stop();
+    }
+
+    private void runMainApplication() throws IOException {
+        Platform.runLater(() -> loadingLabel.setText("Launching application..."));
+        try {
+            System.out.println("Starting main application");
+            String classPath = File.pathSeparator + "lib" + File.separator + "*";
+            String command = String.format("java -ea -cp %s address.MainApp", classPath);
+            Runtime.getRuntime().exec(command, null, new File(System.getProperty("user.dir")));
+            System.out.println("Main application launched");
+        } catch (IOException e) {
+            throw new IOException(ERROR_RUNNING, e);
+        }
+    }
+
+    private void runInstaller() throws IOException {
         Platform.runLater(() -> loadingLabel.setText("Installing..."));
         try {
+            Installer installer = new Installer();
             installer.runInstall(loadingLabel, progressBar);
         } catch (IOException e) {
             throw new IOException(ERROR_INSTALL, e);
         }
+    }
 
-        Platform.runLater(() -> loadingLabel.setText("Launching AddressBook..."));
-        try {
-            startMainApplication();
-        } catch (IOException e) {
-            throw new IOException(ERROR_RUNNING, e);
-        }
+    /**
+     * Determines if installer should be run
+     *
+     * Installer should only be run only if there is no existing installation or existing installation has the
+     * matching version as the installer
+     *
+     * Assumes that the application is not installed yet if version data file cannot be found
+     *
+     * @return
+     * @throws IOException
+     */
+    private boolean shouldRunInstaller() throws IOException {
+        Optional<VersionData> currentVersionData = getCurrentVersionData();
+        if (!currentVersionData.isPresent()) return true;
+        VersionData packedVersionData = getPackedVersionData();
+        Version currentVersion = Version.fromString(currentVersionData.get().getVersion());
+        Version packedVersion = Version.fromString(packedVersionData.getVersion());
+        return packedVersion.compareTo(currentVersion) == 0;
+    }
 
-        stop();
+    private Optional<VersionData> getCurrentVersionData() throws IOException {
+        File versionDataFile = new File(VERSION_DATA);
+        if (!versionDataFile.exists()) return Optional.empty();
+        return Optional.of(JsonUtil.fromJsonString(FileUtil.readFromFile(versionDataFile), VersionData.class));
+    }
+
+    private VersionData getPackedVersionData() throws IOException {
+        String json = FileUtil.readFromInputStream(Installer.class.getResourceAsStream(VERSION_DATA_RESOURCE));
+        return JsonUtil.fromJsonString(json, VersionData.class);
     }
 
     private void showErrorDialogAndQuit(String title, String headerText, String contentText) {
@@ -72,17 +120,6 @@ public class Launcher extends Application {
             alert.showAndWait();
             stop();
         });
-    }
-
-    private void startMainApplication() throws IOException {
-        System.out.println("Starting main application");
-
-        String classPath = File.pathSeparator + "lib" + File.separator + "*";
-        String command = String.format("java -ea -cp %s address.MainApp", classPath);
-
-        Runtime.getRuntime().exec(command, null, new File(System.getProperty("user.dir")));
-
-        System.out.println("Main application launched");
     }
 
     private void showWaitingWindow(Stage stage) {
