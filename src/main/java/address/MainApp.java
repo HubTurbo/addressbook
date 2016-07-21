82259e3a1dc95320b26b294dc5f09ac4667895fc
m133225 <m133225@hotmail.com>
2016-07-21 16:19:32 +0800

    V1.6.1ea

--- src/main/java/address/MainApp.java
+++ src/main/java/address/MainApp.java
@@ -26,7 +26,7 @@ public class MainApp extends Application {
 
     private static final int VERSION_MAJOR = 1;
     private static final int VERSION_MINOR = 6;
-    private static final int VERSION_PATCH = 0;
+    private static final int VERSION_PATCH = 1;
     private static final boolean VERSION_EARLY_ACCESS = true;
 
     public static final commons.Version VERSION = new commons.Version(
