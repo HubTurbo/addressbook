09570e6e2f577505a94da3747385ff1fdfdb760b
You Liang <a0125975@u.nus.edu>
2016-07-21 11:51:58 +0800

    test KeyBindingsGuiTest

--- src/test/java/address/testutil/TestUtil.java
+++ src/test/java/address/testutil/TestUtil.java
@@ -205,7 +205,7 @@ public class TestUtil {
     public static void captureScreenShot(String fileName) {
         File file = GuiTest.captureScreenshot();
         try {
-            Files.copy(file, new File(fileName));
+            Files.copy(file, new File(fileName + ".png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
